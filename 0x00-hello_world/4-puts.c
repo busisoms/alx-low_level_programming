cbdba5645319e143797d87c39e050434ece287a8
busisoms <busisondumo1@gmail.com>
2023-09-07 22:16:56 +0000

    Edit, 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 # include <stdio.h>
-/*
+/**
 * main - prints "programming is like building a multilingual puzzle
 *
-* Return: - (0) success
+* Return: Always (0) success
 */
 
-int /* programming is like building a multilingual puzzle*/ main(void)
+int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
