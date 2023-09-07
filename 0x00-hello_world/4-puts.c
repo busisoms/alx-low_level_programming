f7188adabd08a2f44243e52ffeb206e9b87744d6
busisoms <busisondumo1@gmail.com>
2023-09-07 21:33:12 +0000

    Finished c programs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+/*
+* main - prints "programing is like building a multilingual puzzle
+*
+* Return: - (0) success
+ */
+
+int /* programing is like building a multilingual puzzle*/main(void)
+{
+	puts("\"programing is like building a multilingual puzzle");
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/*
+* main - prints exactly with proper grammar, but the outcome is a piece of art,
+*
+* Return: (0) success
+*/
+
+int /* prints sentence exactly*/ main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
