f4ca0379bb0970ed56d3492780524866a807a277
Busiso <busisondumo1@gmail.com>
2023-09-13 04:50:16 +0200

    Finished task6 - task7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _abs - computes the absolute value of an integer
+* @n: any given number
+* Return: number if positive
+* else return number *-1
+*/
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_last_digit - prints the last digit of a number
+* @n: any given number
+*
+* Return: last digit of a number
+*/
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+	return (last_digit);
+}
