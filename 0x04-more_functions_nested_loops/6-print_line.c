df80e15bf5e835d5420649abb1a192462b635053
Busiso <busisondumo1@gmail.com>
2023-09-15 23:03:48 +0200

    Fix 3-print_remaining_days.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_line - draws a line in the terminal
+* @n: number of (_)
+*
+*/
+
+void print_line(int n)
+{
+	int m;
+
+	if (n != 0)
+	{
+		for (m = 0; m <= n; m++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
