bdbaaaeded7eefd32ea614a77c0e8d24ce6e13a7
Busiso <busisondumo1@gmail.com>
2023-10-02 23:36:42 +0200

    Fix bug where argc is 1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - multiplies two numbers
@@ -14,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	int i, j, product = 1;
 
-	if (argc <= 1)
+	if (argc != 3)
 	{
 		printf("%s\n", "Error");
 		return (1);
