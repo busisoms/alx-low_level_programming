ea48cde92019ef41a82d8a43e076a93322d20d15
Busiso <busisondumo1@gmail.com>
2023-11-02 03:04:34 +0200

    Finish 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *argstostr - concatenates all the arguments of your program
+ * @ac: size of array @av
+ * @av: array of strings
+ *
+ * Return: pointer to new string, otherwise return NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, len = 0;
+	int total_len = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	
+	for (i = 0; i < ac; i++)
+	{
+		len = strlen(av[i]);
+		total_len += len + 1;
+	}
+	str = malloc(total_len * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0, j = 0; i < ac; i++)
+	{
+		len = strlen(av[i]);
+		strcpy(str + j, av[i]);
+		j += len;
+		str[j] = '\n';
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -4,3 +4,7 @@
 - `2-str_concat.c` function that concatenates two strings
 - `3-alloc_grid.c` function that returns a pointer to a 2 dimensional array of integers
 - `4-free_grid.c` function that frees a 2 dimensional grid previously created by your alloc_grid function.
+
+**Advance**
+
+- `100-argstostr.c` concatenates all the arguments of your program
