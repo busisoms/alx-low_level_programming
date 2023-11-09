2070e45e756d2a56b805230bfae7578eae4f3eb7
Busiso <busisondumo1@gmail.com>
2023-11-09 02:27:46 +0200

    fix possible errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,6 +9,10 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+	{
+		return;
+	}
 	f(name);
 }
 
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
@@ -27,14 +27,14 @@ int main(int argc, char **argv)
 	if (cal == NULL)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 
 
 	if (num2 == 0 && (operator[0] == '/' || operator[0] == '%'))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 
 
