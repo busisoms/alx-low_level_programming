6fe61d00427c368b3c08963515e5cb815e1dd1ae
Busiso <busisondumo1@gmail.com>
2023-11-10 04:00:07 +0200

    Done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum of all its parameters
+ * @n: number of args
+ *
+ * Returns: returns the sum of all its parameters, otherwise return 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	unsigned int sum = 0;
+	unsigned int add;
+
+	va_start(args, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		add = va_arg(args, int);
+		sum += add;
+	}
+	va_end(args);
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,71 @@
+#include "variadic_functions.h"
+
+/**
+ * pick_data - choose datatype and prints accordly
+ * @data: chosen datatype
+ * @args: args
+ *
+ */
+
+void pick_data(char data, va_list args)
+{
+	char *str;
+
+	switch (data)
+	{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%.2f", va_arg(args, double));
+			break;
+		case 's':
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					printf("(nil)");
+				else
+					printf("%s", str);
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+/**
+ * print_all - prints anything
+ * @format:  list of types of arguments passed to the function
+ */
+
+void print_all(const char *const format, ...)
+{
+	int count = 0, i = 0;
+	va_list args;
+	char data;
+
+	while (format[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+
+	va_start(args, format);
+
+	i = 0;
+	while (i < count && format[i] != '\0')
+	{
+		data = format[i];
+		pick_data(data, args);
+		i++;
+		if (i < count && format[i] != '\0' &&
+(format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+			printf(", ");
+	}
+
+	printf("\n");
+	va_end(args);
+}
