f477c27003c739a8a2a94c88b296f15702f3995d
Busiso <busisondumo1@gmail.com>
2023-11-17 02:09:29 +0200

    betty warning

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,7 +4,7 @@
  * add_node_end - adds a new node at the end of a list_t list
  * @head: pointer that points to a node
  * @str: new data that is a string
- * Return : address of the new element, or NULL if it failed
+ * Return: address of the new element, or NULL if it failed
  */
 
 list_t *add_node_end(list_t **head, const char *str)
