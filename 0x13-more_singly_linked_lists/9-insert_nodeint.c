71df6cb1ab764d676dd38bd8023c753c2742208f
Busiso <busisondumo1@gmail.com>
2023-11-20 23:20:00 +0200

    fix bugs in task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,6 +34,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 	}
 
+	if (current == NULL)
+	{
+		return (NULL);
+	}
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
