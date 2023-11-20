7c298198c42c6ca9a56ced33bb9c179f3ac00a74
Busiso <busisondumo1@gmail.com>
2023-11-20 23:25:53 +0200

    fix bugs in task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		return (*head);
+		return (add_nodeint_end(head, n));
 	}
 
 	current = *head;
