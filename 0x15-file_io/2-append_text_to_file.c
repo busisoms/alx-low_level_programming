4a5d79960f8d336d147008f7f079951ce757e8aa
Busiso <busisondumo1@gmail.com>
2023-11-28 04:19:09 +0200

    Fix betty error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: name of the file
- * @text_contents: contexts to be appended
+ * @text_content: contexts to be appended
  *
  * Return: 1 on success and -1 on failure
  */
