0180f70a9c40be115f23f76da32f608ce6be8700
Busiso <busisondumo1@gmail.com>
2024-04-13 05:00:05 +0200

    (Fix:) betty

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,15 +8,18 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *node, *temp;
+
 	if (ht == NULL)
 		return;
 
-	for (unsigned long int i = 0; i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		hash_node_t *node = ht->array[i];
+		node = ht->array[i];
 		while (node != NULL)
 		{
-			hash_node_t *temp = node;
+			temp = node;
 			node = node->next;
 			free(temp->key);
 			free(temp->value);
