ef08a6e1d7fc309973f817104efefdc49772802a
Busiso <busisondumo1@gmail.com>
2025-03-18 22:38:13 +0200

    Add: Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: string to be printed
+ *
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, index;
+	char *cpy_str = str;
+
+	while (*cpy_str != '\0')
+	{
+		len++;
+		cpy_str++;
+	}
+
+	if (len % 2 == 0)
+	{
+		index = len / 2;
+		while (index <= len)
+		{
+			_putchar(str[index]);
+			index++;
+		}
+	}
+	else
+	{
+		index = (len - 1) / 2;
+		while (index <= len)
+		{
+			_putchar(str[index]);
+			index++;
+		}
+	}
+	_putchar('\n');
+}
+
